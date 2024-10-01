b4ca9557255e13b8760ed2955e2fbc8892d481c9
unknown <felipengueira1@gmail.com>
2024-10-01 17:22:59 -0300

    Introdução API

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express')
-const app = express();
+const app = express()
 
-const port = 3000;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+let personList = []
+
+const port = 3000
+
+app.get('/visualizar', (req, res) => {
+    res.send(personList)
+})
+
+app.get('/params/:id', (req, res) =>{
+    const { id } = req.params
+
+    res.send(id)
+})
+
+app.post('/cadastrar', (req, res) => {
+    const { name, age} = req.body
+    personList.push({name, age})
+    res.send(`Usuário recebido!!! nome do usuário ${name}`)
+})
+
+app.delete('/deletar', (req, res) =>{
+    const {name, age} = req.body
+    personList.pop()
+    res.send(`Usuário deletado com sucesso! ${name}`)
+})
+
+app.put('/update', (req, res) =>{
+    const{name, age} = req.body
+    personList.put()
+    res.send(`Dados atualizados com sucesso! ${name}`)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+console.log("oii")
